ae643e29da3e3bc7a191de5eff44efb9a556091b
Ahmad Muslih <72424284+Ahmadmuslih123@users.noreply.github.com>
2021-12-12 22:41:28 +0700

    Add files via upload

---
+++ Pertemuan 3/Hello World/app/src/main/java/com/AhmadMuslihSyafii_19104034/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.musliih_19104034.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
